1d6b3e42c10ea0e10ceb8b3a35c5048fede04d9c
rosachen3 <rosa.chen3@hotmail.com>
2024-06-14 17:54:39 -0400

    style: Styled die face

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
+import React from 'react'
 import './index.css'
+import Die from "./components/Die"
 
 export default function App() {
-  return (
-    <main>
-       
+	const diceData = [1,2,3,4,5,6,1,2,3,4]
+	const diceElements = diceData.map(die => {
+		return <Die value={die} />
+	})
+	
+	return (
+    <main className="game-container">
+		<div className="dice-container">
+			{diceElements}
+		</div>
     </main>
   )
 }
---
+++ app/src/components/Die.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Die(props) {
+    return (
+        <div className="die">
+            <h2>{props.value}</h2>
+        </div>    
+    )
+}
