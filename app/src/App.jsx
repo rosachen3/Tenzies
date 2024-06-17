bfbf241f4a5bca439804ac7d2a44e1c62381ad5f
rosachen3 <rosa.chen3@hotmail.com>
2024-06-17 15:34:43 -0400

    feat: Added random dice roll button

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,18 +1,34 @@
 import React from 'react'
+import { useState } from 'react'
 import './index.css'
 import Die from "./components/Die"
 
 export default function App() {
-	const diceData = [1,2,3,4,5,6,1,2,3,4]
+	const [diceData, setDiceData] = useState(generateRandomDice(1,6,10))
+	const numDice = 10
 	const diceElements = diceData.map(die => {
 		return <Die value={die} />
 	})
 	
+	function generateRandomDice(min, max, numDice) {
+		const rolls = []
+		for (let i=0; i<numDice; i++){
+			let randomNum = Math.floor(Math.random() * max) + 1
+			rolls.push(randomNum)
+		}
+		return rolls
+	}
+
+	function handleReroll() {
+		setDiceData(generateRandomDice(1,6,numDice))
+	}
+
 	return (
     <main className="game-container">
 		<div className="dice-container">
 			{diceElements}
 		</div>
+		<button className="roll" onClick={handleReroll}>Roll</button>
     </main>
   )
 }
