c38056a210caa40c3f676405cc7833578a567e58
rosachen3 <rosa.chen3@hotmail.com>
2024-06-19 16:12:35 -0400

    feat: save dice on reroll

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,25 +2,55 @@ import React from 'react'
 import { useState } from 'react'
 import './index.css'
 import Die from "./components/Die"
+import { nanoid } from "nanoid"
 
 export default function App() {
 	const [diceData, setDiceData] = useState(generateRandomDice(1,6,10))
 	const numDice = 10
 	const diceElements = diceData.map(die => {
-		return <Die value={die} />
+		return <Die 
+			key={die.id}
+			value={die.value}
+			isHeld={die.isHeld}
+			holdDice={() => holdDice(die.id)}
+			/>
 	})
-	
+
 	function generateRandomDice(min, max, numDice) {
 		const rolls = []
 		for (let i=0; i<numDice; i++){
 			let randomNum = Math.floor(Math.random() * max) + 1
-			rolls.push(randomNum)
+			const diceObject = {
+				id: nanoid(),
+				value: randomNum,
+				isHeld: false
+			}
+			rolls.push(diceObject)
 		}
 		return rolls
 	}
 
+	function generateNewDie() {
+		let randomNum = Math.floor(Math.random() * 6) + 1
+		return {
+			id: nanoid(),
+			value: randomNum,
+			isHeld: false
+		}
+	}
+
 	function handleReroll() {
-		setDiceData(generateRandomDice(1,6,numDice))
+		setDiceData(oldDice => oldDice.map(die => {
+			return die.isHeld ? 
+				die : generateNewDie()
+		}))
+	}
+
+	function holdDice(id) {
+		setDiceData(oldDice => oldDice.map(die => {
+			return die.id === id ? 
+				{...die, isHeld: !die.isHeld} : die
+		}))
 	}
 
 	return (
--- app/src/components/Die.jsx
+++ app/src/components/Die.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
 export default function Die(props) {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
+    
     return (
-        <div className="die">
+        <div 
+            className="die" 
+            style={styles}
+            onClick={props.holdDice}
+        >
             <h2>{props.value}</h2>
         </div>    
     )
